d56a04f5eea49644c1130efefc577fa6c2dbfbf7
Hanamant <hanamant.kattimani@mountblue.tech>
2020-11-12 16:31:58 +0530

    Added one function matchesWonPerTeamPerYear() to get number of matches won per team per year

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const ipl= require('./ipl')
 let perYearMatchCount={}
+let objectToStoreOutput={}
 fs.createReadStream('../data/matches.csv')
   .on('error',()=>{
     console.log('Error has occured')
@@ -10,8 +11,10 @@ fs.createReadStream('../data/matches.csv')
   .on('data', (row) => {
 
     perYearMatchCount=ipl.matchesPerYear(row,perYearMatchCount)
+    objectToStoreOutput=ipl.matchesWonPerTeamPerYear(row,objectToStoreOutput)
   })
   .on('end', () => {
-
+    console.log("\n\n\nMatch count per year   :\n",perYearMatchCount)
     console.log(perYearMatchCount);
+    console.log("\n\n\nNumber of matches own per year per team   :\n",objectToStoreOutput)
   });
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -7,5 +7,15 @@ function matchesPerYear(row,perYearMatchCount){
     return perYearMatchCount
 }
 
-module.exports={matchesPerYear}
+
+function matchesWonPerTeamPerYear(row,objectToStoreOutput)
+{
+  if(objectToStoreOutput.hasOwnProperty(row['season']+"  "+row['winner']))
+  objectToStoreOutput[row['season']+"  "+row['winner']]++
+   else
+   objectToStoreOutput[row['season']+"  "+row['winner']]=1
+   return objectToStoreOutput
+}
+
+module.exports={matchesPerYear,matchesWonPerTeamPerYear}
 
