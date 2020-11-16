9d9af27b304e29769dbb631d2f6b8c33204b0f21
Hanamant <hanamant.kattimani@mountblue.tech>
2020-11-16 13:02:32 +0530

    added new methods econimicalBowlerInIpl

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,72 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const ipl= require('./ipl')
+
 let perYearMatchCount={}
 let objectToStoreOutput={}
-let year='2016'
-let extraRun={}
+let extra_runs={}
+let matchIdInYear={}
+let allBowlerInYear={}
+let economicalBowler={}
+
+
 fs.createReadStream('../data/matches.csv')
   .on('error',()=>{
     console.log('Error has occured')
     })
   .pipe(csv())
   .on('data', (row) => {
-
+    //que1
     perYearMatchCount=ipl.matchesPerYear(row,perYearMatchCount)
+    
+    //que2
     objectToStoreOutput=ipl.matchesWonPerTeamPerYear(row,objectToStoreOutput)
+    //que3
+    matchIdInYear=ipl.getId(row,matchIdInYear,'2016','winner')  
+    extra_runs=matchIdInYear
 
-    extraRun=ipl.extraRunPerTeamInYear(row,year,extraRun)
+    //que4 
+    matchIdInYear=ipl.getId(row,matchIdInYear,'2016','winner') 
+    allBowlerInYear=matchIdInYear
 
   })
   .on('end', () => {
-    console.log("\n\n\nMatch count per year   :\n",perYearMatchCount)
-    console.log(perYearMatchCount);
-    console.log("\n\n\nNumber of matches won per year per team   :\n",objectToStoreOutput)
-    console.log("\n\n\nExtra run conceded Per team in year 2016   :\n",extraRun)
+
+                  fs.createReadStream('../data/deliveries.csv')
+                  .pipe(csv())
+                  .on('data', (row1) => {
+                    //que3
+                    extra_runs=ipl.extraRunConceded(row1,extra_runs)
+
+                    //que4
+                    if(allBowlerInYear.hasOwnProperty(row1['match_id'])){
+                      if(economicalBowler.hasOwnProperty(row1['bowler']))
+                        economicalBowler[row1['bowler']]+=parseInt(row1['total_runs'])
+                      else
+                        economicalBowler[row1['bowler']]=0
+                      }
+                   
+                  })
+                  .on('end', () => {
+                    
+                    ipl.storeDataToJSON(extra_runs,'../public/output/extraRunConceded.json')
+
+                    let topTenEconomicalBowler=ipl.topTen(economicalBowler)
+                    ipl.storeDataToJSON(topTenEconomicalBowler,'../public/output/topTenEconomicalBowler.json')
+                    
+                    
+                  });
+
+    
+    ipl.storeDataToJSON(perYearMatchCount,'../public/output/matchesPerYear.json')
+    
+    ipl.storeDataToJSON(objectToStoreOutput,'../public/output/matchesWonPerTeamPerYear.json')
+    
   });
+
+ 
+  
+
+
+
+
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,4 +1,4 @@
-
+const fs=require('fs')
 function matchesPerYear(row,perYearMatchCount){
     if(perYearMatchCount.hasOwnProperty(row['season']))
         perYearMatchCount[row['season']]++
@@ -30,5 +30,53 @@ function extraRunPerTeamInYear(row,year,extraRun)
   return extraRun
 }
 
-module.exports={matchesPerYear,matchesWonPerTeamPerYear,extraRunPerTeamInYear}
+
+
+function getId(row,matchIdInYear,year,column)
+{
+  if(row['season']==year)
+    {  
+        if(!(matchIdInYear.hasOwnProperty(row[column])))
+          matchIdInYear[row['id']] = 0
+    }
+    return matchIdInYear
+}
+
+function extraRunConceded(row1,extra_runs)
+{
+if(extra_runs.hasOwnProperty(row1['match_id']))
+extra_runs[row1['match_id']]+=parseInt(row1['extra_runs'])
+return extra_runs
+}
+
+
+
+function topTen(economicalBowler)
+{
+  var sort_economicalBowler = [];
+  for (var bowler in economicalBowler) {
+    sort_economicalBowler.push([bowler, economicalBowler[bowler]]);
+    }
+
+    sort_economicalBowler.sort(function(a, b) {
+      return a[1] - b[1];
+  });
+  
+  
+  return sort_economicalBowler
+}
+
+
+
+const storeDataToJSON = (str, path) => {
+    try {
+      fs.writeFileSync(path, JSON.stringify(str))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+
+
+module.exports={matchesPerYear,matchesWonPerTeamPerYear,extraRunPerTeamInYear,getId,extraRunConceded,topTen,storeDataToJSON}
 
