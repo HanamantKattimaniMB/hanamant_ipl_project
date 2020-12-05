c50c8f6dff9a041de2d944b57816d258a11bdb90
Hanamant <hanamant.kattimani@mountblue.tech>
2020-12-05 19:06:54 +0530

    Modified old modules by using sql query

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,152 +1,108 @@
-const fs=require('fs')
-function matchesPerYear(match_data){
-    const output={}
-    match_data.map((match)=>{
-        if(output.hasOwnProperty(match['season'])){
-            output[match['season']]+=1
-        }
-        else{
-            output[match['season']]=1
-        }
+const envVariable=require('./config')
+var mysql=require('mysql');
+
+var connection=mysql.createConnection({
+  host     : envVariable.DB_HOST,
+  user     : envVariable.DB_USER,
+  password : envVariable.DB_PASS,
+  database : envVariable.DB_NAME,
+})
+
+connection.connect()
+
+connection.query('USE ipl',(error) => {
+    if(error){
+      throw error;
+    } 
+  });
+
+let createTableDeliveries = 'CREATE TABLE deliveries(match_id INT,inning INT,batting_team VARCHAR(60),bowling_team VARCHAR(60),over INT,ball INT,batsman VARCHAR(60),non_striker VARCHAR(60),bowler VARCHAR(60),is_super_over BOOLEAN,wide_runs INT,bye_runs INT,legbye_runs INT, noball_runs INT, penalty_runs INT, batsman_runs INT, extra_runs INT, total_runs INT, player_dismissed VARCHAR(60), dismissal_kind VARCHAR(60), fielder VARCHAR(60));'
+let createTableMatches = 'CREATE TABLE matches(Id INT PRIMARY KEY,season INT,city VARCHAR(20),date DATE,team1 VARCHAR(30) NOT NULL,team2 VARCHAR(30) NOT NULL,toss_winner VARCHAR(30),toss_decision VARCHAR(10),result VARCHAR(10),dl_applied BOOLEAN,winner VARCHAR(30),win_by_runs INT,win_by_wickets INT,player_of_match VARCHAR(30), venue VARCHAR(50),umpire1 VARCHAR(30),umpire2 VARCHAR(30),umpire3 VARCHAR(30));'
+
+function creatTable(createTableDeliveries, createTableMatches){
+  connection.query(createTableDeliveries,(error) => {
+    if(error){
+      throw error;
+    } 
   })
-    return output
+  connection.end
 }
 
-function matchesWonPerTeamPerYear(match_data){
-    let output={}
-    let teams=new Set()
-    let seasons=new Set()
-
-      match_data.map((match)=>{
-        if(match['winner']!=''){
-          teams.add(match['winner'])  
-        }else{
-            //team name is empty
-        }
-        seasons.add(match['season'])
-
-        if(output[match['season']]){
-            if(output[match['season']][match['winner']]){
-                output[match['season']][match['winner']]+=1
-            }
-            else{
-                output[match['season']][match['winner']]=1
-            }   
-        }
-        else{   
-            output[match['season']]={}
-            output[match['season']][match['winner']]=1
-        }
-    })
-  
-    let sortedSeasons=Array.from(seasons).sort((a,b)=>{return a-b})
-    let sortedTeams=Array.from(teams).sort((a,b)=>{return a-b})
-
-    let outputObj={}
-    for(let season of sortedSeasons){
-        outputObj[season]=[]
-        for(let team of sortedTeams){
-            if(output[season][team]){
-                outputObj[season].push(output[season][team])
-            }
-            else{
-                outputObj[season].push(0)
-            }
-        }
-    }
-
-    let matchesWithSeasonAndWin=[]
-    let teamData={}
-    for(let year in outputObj){
-        teamData['name']=year
-        teamData['data']=outputObj[year]
-        matchesWithSeasonAndWin.push(teamData)
-        teamData={}
-    }
-  
-    let mainOutput={}
-    mainOutput['teams']=sortedTeams
-    mainOutput['matches']=matchesWithSeasonAndWin
-    return mainOutput
-}
-
-
-function extraRunConceded(match_data,delivery_data,year)
-{
-    let filtered_match_data=match_data.filter(match=>match.season==year)
-    const output={}
-      filtered_match_data.map((match)=>{
-           delivery_data.map((delivery)=>{
-            if(match['id']==delivery['match_id']){
-                if(output[delivery.bowling_team]){
-                    output[delivery['bowling_team']]+=parseInt(delivery['extra_runs'])
-                }else{
-                    output[delivery['bowling_team']]=parseInt(delivery['extra_runs'])
-                }
-            }
+function  loadCSV(){
+    return new Promise((resolve,reject) => {
+      let loadMatchesData=`LOAD DATA LOCAL INFILE '/home/lakan/MB/testing/hanamant_ipl_project/src/data/matches.csv' INTO TABLE matches FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;`
+      let loadDeliveriesData=`LOAD DATA LOCAL INFILE '/home/lakan/MB/testing/hanamant_ipl_project/src/data/deliveries.csv' INTO TABLE deliveries FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;`
+      connection.query(loadDeliveriesData, function (error, matchesOwnPerTeamPerYear, fields) {
+          if(error){
+             return reject(error)
+          } else{
+            return resolve(matchesOwnPerTeamPerYear)
+          }
         })
+        connection.end
     })
-    return output
-}
-
-
-
-function topTenEconomicalBowlerInSeason(match_data,delivery_data,year){
-    let output={}
-    let filtered_match_data=match_data.filter(match=>match.season==year)
-    filtered_match_data.map((match)=>{
-            delivery_data.map((delivery)=>{    
-            if(match['id']==delivery['match_id']){
-                if(output.hasOwnProperty(delivery['bowler'])){
-                    output[delivery['bowler']]['run']+=parseInt(delivery['total_runs'])
-                    if((delivery['wide_runs']==0) && (delivery['noball_runs']==0)){
-                        output[delivery['bowler']]['bowls']++
-                    }
-                }
-                else{
-                    output[delivery['bowler']]={}
-                    output[delivery['bowler']]['run']=parseInt(delivery['total_runs'])
-                    if((delivery['noball_runs']==0) && (delivery['wide_runs']==0)){
-                        output[delivery['bowler']]['bowls']=1
-                    }
-                    else{
-                        output[delivery['bowler']]['bowls']=0
-                    }   
-                }
-            }
-        }) 
-    })
-  
-    let arrayOfBowlersAndEconomyRate=[]
-    for(let bowler in output){
-        let over=output[bowler].bowls/6
-        let runs= output[bowler].run
-        economicalRate=runs/over
-        arrayOfBowlersAndEconomyRate.push([bowler,economicalRate])
-    }
-   
-    let sortedArrayOfBowlersAndEconomyRate=arrayOfBowlersAndEconomyRate.sort((a,b)=>{
-        return a[1]-b[1]
-    })
-
-    let topTenEconomicalBowler=[]
-    topTenEconomicalBowler= sortedArrayOfBowlersAndEconomyRate.slice(0,10)
-    let topTenEconomicalBowler_obj=Object.assign({}, topTenEconomicalBowler)
-    return topTenEconomicalBowler_obj
-}
-
-const storeDataToJSON = (str, path) => {
-    try {
-      fs.writeFileSync(path, JSON.stringify(str))
-    } catch (err) {
-      console.error('error is occurred at writing file',err)
-    }
   }
+ 
+
+function  matchesPerYear_sql(){
+  return new Promise((resolve,reject) => {
+    connection.query('SELECT season,count(season) as numberOfMatches FROM matches GROUP BY season', function (error, matchesPerYear) {
+        if(error){
+           return reject(error)
+        } else{
+          return resolve(matchesPerYear)
+        }
+      })
+      connection.end
+
+  })
+}
+
+function  matchesOwnPerTeamPerYear_sql(){
+  return new Promise((resolve,reject) => {
+    connection.query('SELECT season,winner,COUNT(winner) as numberOfMatches  FROM matches WHERE winner!="" GROUP BY season,winner ORDER BY season;', function (error, matchesOwnPerTeamPerYear, fields) {
+        if(error){
+           return reject(error)
+        } else{
+          return resolve(matchesOwnPerTeamPerYear)
+        }
+      })
+      connection.end
+  })
+}
+
+function  extraRunConceded_sql(){
+  return new Promise((resolve,reject)=>{
+    connection.query('SELECT d.bowling_team, SUM(d.extra_runs) as extra_runs FROM deliveries d JOIN matches m ON m.id = d.match_id WHERE m.season = 2016 GROUP BY d.bowling_team', function (error, extraRunConceded, fields) {
+        if(error){
+           return reject(error)
+        } else{
+          return resolve(extraRunConceded)
+        }
+      })
+      connection.end
+  })
+}
+
+
+function  topTenEconomicalBowler_sql(){
+  return new Promise((resolve,reject)=>{
+    connection.query('SELECT bowler, ((runs*1.0)/(overs*1.0))AS over_runs FROM(SELECT bowler , ((count(ball) *1.0) /(6 * 1.0))AS overs, SUM(total_runs)AS runs FROM deliveries JOIN matches ON deliveries.match_id = matches.id  WHERE matches.season = 2015 AND wide_runs=0 AND noball_runs = 0 GROUP BY bowler)inter ORDER BY over_runs LIMIT 10;', function (error, topTenEconomicalBowlers) {
+        if(error){
+           return reject(error)
+        } else{
+          return resolve(topTenEconomicalBowlers)
+        }
+      })
+      connection.end
+  })
+}
 
 module.exports={
-    matchesPerYear,
-    matchesWonPerTeamPerYear,
-    extraRunConceded,
-    topTenEconomicalBowlerInSeason,
-    storeDataToJSON
+  matchesPerYear_sql,
+  matchesOwnPerTeamPerYear_sql,
+  extraRunConceded_sql,
+  topTenEconomicalBowler_sql,
 }
+
+
